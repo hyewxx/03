699dd1afd6433f06ff910c2c82b89bc0442cabd3
hyewxx <hyeww0408@sookmyung.ac.kr>
2026-09-17 16:34:21 +0900

    third submission

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main(void) {
     char c;
     int i;
 
-    printf("input a number :");     // 5를 입력하면 c에 문자 '5'가 입력, '5'는 아스키 코드 53
+    printf("input a character : ");
     scanf("%c", &c);
 
-    i = c-48;
-    printf("The input number is %i\n", i);
+    i = c + 1;
+    printf("The next character of %c (%d) : %c", c, c, i);
 
     return 0;
 }
